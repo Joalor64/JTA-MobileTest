e9bf547833452d2f0713e00d3f77b1a28d665291
Joalor64 <rhgarcon@gmail.com>
2026-08-27 14:56:02 -0400

    bro

--- source/jta/mobile/MobileInput.hx
+++ source/jta/mobile/MobileInput.hx
@@ -102,7 +102,7 @@ class MobileInput extends FlxTypedGroup<VirtualButton>
 		addButton(margin, Flx.height - margin - 64, 'down', 'navigation');
 
 		addButton(FlxG.width - margin - 140, FlxG.height - margin - 64, 'cancel');
-		addButton(FlxG.width - margin - 64, FlxG.height - margin - 140, confirm');
+		addButton(FlxG.width - margin - 64, FlxG.height - margin - 140, 'confirm');
 	}
 
 	/**
